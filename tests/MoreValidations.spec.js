ef543e38b40b51a012e70d1d54b73d2ff9775780
Ashishkr403 <ashishkr403@gmail.com>
2026-04-08 00:28:57 +0530

    visual testing done

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -4,7 +4,9 @@ test("Calendar validations", async ({ page }) => {
 
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#displayed-text").screenshot({ path: "Partialscreenshot.png" });
     await page.locator("#hide-textbox").click();
+    await page.screenshot({ path: "screenshot.png"});
     await expect(page.locator("#displayed-text")).toBeHidden();
     page.on("dialog", dialog => dialog.accept());
     await page.locator("#confirmbtn").click();
@@ -17,3 +19,15 @@ test("Calendar validations", async ({ page }) => {
 
     await page.pause();
 });
+
+// screenshot - store  - screenshot 
+
+test("visual testing", async ({ page }) => {
+
+    await page.goto("https://www.flightaware.com/");
+
+
+    expect(await page.screenshot()).toMatchSnapshot('flightaware.png');
+});
+
+    
