d3a360e4b220fc87ef7787bfd07c7652687e9b6b
Ashishkr403 <ashishkr403@gmail.com>
2026-04-09 00:54:03 +0530

    excelTest

---
+++ exceljs/exceldemo.js
@@ -0,0 +1,24 @@
+const exceljs = require ('exceljs');
+
+
+async function excelTest(params) {
+    
+const workbook = new exceljs.Workbook();
+await workbook.xlsx.readFile("C:\\Users\\hp\\Downloads\\ExceldownloadTest.xlsx");
+    const worksheet = workbook.getWorksheet('Sheet1');
+    worksheet.eachRow((row, rowNumber) => 
+    {
+    row.eachCell((cell, colNumber) =>
+         {
+
+
+        if (cell.value === "Winter") {
+            console.log("Winter is found in row " + rowNumber + " and column " + colNumber);
+        }
+        //console.log(cell.value);
+
+        })
+    })
+}
+
+excelTest();
