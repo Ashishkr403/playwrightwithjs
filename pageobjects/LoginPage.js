a7e61d0d30c1d454fab8afc4c0f7704b8934fce4
Ashish Kumar <Ashishkr403@gmail.com>
2026-05-15 11:13:37 +0530

    loginPage update

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -3,9 +3,10 @@ class LoginPage {
     constructor(page) {
         this.page = page; // Store the page object for later use and ab isko class ke andar khi bhi use kr sakte ager asa nhi krenge to goto method ko use nhi kr payege
 
-        this.signInButton = page.getByRole('button', { name: 'Login' });
+        
         this.userName = page.locator("#userEmail");
         this.password = page.locator("#userPassword");
+        this.signInButton = page.getByRole('button', { name: 'Login' });
     
     }
     async goTo() {
